51644491f9a559c556915d4c73211c318a21eb0b
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-03-07 13:27:00 -0500

    My commit message

---
+++ index.js
@@ -0,0 +1,18 @@
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+  }
+
+  function introduction(name) {
+    return `Hi, my name is ${name}.`;
+  }
+
+
+
+
+
+
+
